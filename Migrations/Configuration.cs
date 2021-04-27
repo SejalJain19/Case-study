60be25a37c2fb9adc2079137f074f2e26a5e843b
SejalJain19 <jain.19.sejal@gmail.com>
2021-04-27 14:32:55 +0530

    first commit

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,26 @@
+﻿namespace TruYumMVC.Migrations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    using TruYumMVC.Models;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<TruYumMVC.Models.TruYumContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(TruYumMVC.Models.TruYumContext context)
+        {
+            context.Categories.AddOrUpdate(e => e.Name,
+                new Models.Category { Name = "Main-Course" },
+                new Models.Category { Name = "Starters" },
+                new Models.Category { Name = "Snack" }
+            );
+        }
+    }
+}
